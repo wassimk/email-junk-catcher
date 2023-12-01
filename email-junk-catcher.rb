fa67dc8b899c023cced59b02cfa5edac6675bd0e
Wassim Metallaoui <wassim@metallaoui.com>
2023-12-01 10:01:39 -0600

    refactor: update script with help from chatgpt

--- email-junk-catcher.rb
+++ email-junk-catcher.rb
@@ -1,31 +1,33 @@
 #!/usr/bin/env ruby
 
-require 'logger'
-logger = Logger.new('run.log', 'daily')
-logger.debug "Started executing"
-
-PASS = File.read(File.expand_path('.private', __dir__)).strip
-VALID_EMAILS = File.read('valid-emails.conf').split("\n")
-
 require 'net/imap'
-imap = Net::IMAP.new('secure.emailsrvr.com', port: 993, ssl: true)
-imap.login('wassim@metallaoui.com', PASS)
+require 'logger'
 
-imap.select('INBOX.spam')
+begin
+  LOGGER = Logger.new(File.expand_path('run.log', __dir__), 'daily')
+  PASS = File.read(File.expand_path('.private', __dir__)).strip
+  VALID_DOMAINS = File.read(File.expand_path('valid-emails.conf', __dir__)).split("\n")
+  
+  imap = Net::IMAP.new('secure.emailsrvr.com', port: 993, ssl: true)
+  imap.login('wassim@metallaoui.com', PASS)
 
-imap.search(['ALL']).each do |message_id|
-  headers = imap.fetch(message_id, 'BODY.PEEK[HEADER.FIELDS (From Subject)]')[0].attr.values.first
-  next unless VALID_EMAILS.any? { |from| headers =~ /#{from}/ }
+  imap.select('INBOX.spam')
 
-  subject = headers.match(/Subject: (.*)/)[1] rescue nil
-  logger.info "Moving to Inbox: [#{message_id}] #{subject}"
+  imap.search(['ALL']).each do |message_id|
+    data = imap.fetch(message_id, 'BODY.PEEK[HEADER.FIELDS (From)]').first
+    from = data.attr.values.first.strip
+    
+    if VALID_DOMAINS.any? {|valid_email| from.include?(valid_email)}
+      LOGGER.info "Moving to Inbox: [#{message_id}] #{from}"
+  
+      imap.copy(message_id, 'Inbox')
+      imap.store(message_id, '+FLAGS', [:Deleted])
+    end
+  end
 
-  imap.copy(message_id, 'Inbox')
-  imap.store(message_id, '+FLAGS', [:Deleted])
+  imap.close
+  imap.logout
+  imap.disconnect
+rescue => e
+  LOGGER.error "Error encountered: #{e.message}"
 end
-
-imap.close
-imap.logout
-imap.disconnect
-
-logger.debug "Done executing"
